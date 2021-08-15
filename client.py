b63f061b495058d983c4b2dfd0297fb5e015a473
Nguyen Tung <thanhtung4693@gmail.com>
2021-08-16 00:19:32 +0700

    refactor: refactor code

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ import cv2
 import base64
 
 addr = 'http://192.168.0.108:5000/'
-test_url = addr + '/api/v1/ai/detection'
+api_url = addr + '/api/v1/ai/detection_v2'
 content_type = 'image/jpeg'
 headers = {'content-type': content_type}
 file_name = 'Testing/Input/tung.jpg'
@@ -16,20 +16,23 @@ def post_image_v1(img_file):
     # encode image as jpeg
     _, img_encoded = cv2.imencode('.jpg', img)
     # send http request with image and receive response
-    response = requests.post(test_url, data=img_encoded.tostring(), headers=headers)
+    response = requests.post(api_url, data=img_encoded.tostring(), headers=headers)
     # decode response
     print(json.loads(response.text))
 
-# /api/v2/ai/detection
+# /api/v1/ai/detection_v1
 def post_image_v2(img_file):
+    api_url = addr + '/api/v1/ai/detection_v1'
     img = open(img_file, 'rb').read()
-    response = requests.post(test_url, data=img, headers=headers)
+    response = requests.post(api_url, data=img, headers=headers)
     print(response.text)
 
+# /api/v1/ai/detection_v2
 def post_image_v3(img_file):
+    api_url = addr + '/api/v1/ai/detection_v2'
     img = open(img_file, 'rb').read()
     image_data = base64.b64encode(img).decode()
-    response = requests.post(test_url, data=image_data, headers=headers)
+    response = requests.post(api_url, data=image_data, headers=headers)
     print(response.text)
 
 # post_image_v1(file_name)
