09677284fc9890ca96ca1a0e32ef6d513da3bffc
Nguyen Tung <thanhtung4693@gmail.com>
2021-08-21 16:12:45 +0700

    feat: add camera realtime detection feature

--- app.py
+++ app.py
@@ -5,15 +5,23 @@ from utils import Detector
 import cv2
 import Core.Helper as help
 import train_data as train
+from abc import ABC, abstractmethod
 
 # Set directories
 BASE_DIR = "Testing/Input"
 list_imgs = os.listdir("Testing/Input\\")
+detector = Detector(True)
+
+
+def init():
+    global detector
+    if os.path.isfile('./DataBase/DataBase.json'):
+        detector = Detector()
 
 
 def excute():
     # create detector object
-    detector = Detector()
+    # detector = Detector()
 
     # loop over images
     for im in list_imgs:
@@ -33,8 +41,26 @@ def excute():
         cv2.imshow("image", image)
         cv2.waitKey(3)
 
+def detectRealTime():
+    cap = cv2.VideoCapture(0)
+    while True:
+        success, img = cap.read()
+        # convert to RGB
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # get predictions and draw them on image
+        predictions = detector.get_people_names(img, speed_up=False, downscale_by=1)
+        annoted_image = detector.draw_results(img, predictions, (255, 238, 0))
+
+        # convert back to BGR (since using cv2)
+        image = cv2.cvtColor(annoted_image, cv2.COLOR_RGB2BGR)
+
+        cv2.imshow('Webcam', image)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
 
 def main():
+    init()
     path = 'dataset'
     print("\t**********************************************")
     print("\t***  Market Enterprise Vietnam AI Lab  ***")
@@ -44,6 +70,7 @@ def main():
     print("\t c - Crop images in the raw dataset.")
     print("\t t - Training the model.")
     print("\t d - Detect the image.")
+    print("\t v - Detect by the camera.")
     print("\t e - Exit.")
     print("\t ------------------ADD-ON-------------------")
     print("\t x - Export pre-trained model.")
@@ -58,6 +85,8 @@ def main():
         train.training_model('Dataset/Crop', files)
     elif choice == "d":
         excute()
+    elif choice == "v":
+        detectRealTime()
     elif choice == "e":
         return
     elif choice == "x":
